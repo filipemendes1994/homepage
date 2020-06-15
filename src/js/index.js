41a2c59bc1ea228dd416f46d5b972467c84bfc16
Filipe Mendes <filipemendes@Filipes-MacBook-Pro.local>
2020-06-15 02:02:12 +0100

    (fix) iterate over design

--- src/js/index.js
+++ src/js/index.js
@@ -20,3 +20,11 @@ const watchNavbarPosition = () => {
       }
     });
 }
+
+const hoverOrganizationTitle = (evt) => {
+  evt.parentElement.children[0].children[1].style.opacity = 1;
+}
+
+const leaveOrganizationTitle = (evt) => {
+  evt.parentElement.children[0].children[1].style.opacity = 0;
+}
