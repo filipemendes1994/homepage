c05e62eb4882722f1f263a65d627d01fc1a95274
Filipe Mendes <filipemendes@Filipes-MacBook-Pro.local>
2020-06-17 21:14:40 +0100

    (feat) add random quote

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => watchNavbarPosition());
+document.addEventListener("DOMContentLoaded", () => {
+  watchNavbarPosition();
+  randomQuote();
+});
 
 const watchNavbarPosition = () => {
   const navbar = document.getElementsByClassName('navbar')[0];
@@ -27,4 +30,18 @@ const hoverOrganizationTitle = (evt) => {
 
 const leaveOrganizationTitle = (evt) => {
   evt.parentElement.children[0].children[1].style.opacity = 0;
+}
+
+
+const quotes = [
+  "The Internet?  Is that thing still around?",
+  "The Web is like a dominatrix.  Everywhere I turn, I see little buttons ordering me to Submit.",
+  "Any fool can use a computer.  Many do.",
+  "Don’t worry if it doesn’t work right.  If everything did, you’d be out of a job.",
+  "I don’t care if it works on your machine!  We are not shipping your machine!"
+];
+
+const randomQuote = () => {
+  const randomQuoteDiv = document.getElementById('randomQuote');
+  randomQuoteDiv.innerText = quotes[Math.floor(Math.random() * 5)];
 }
