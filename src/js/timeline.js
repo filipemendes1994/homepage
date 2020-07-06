74c51132b2d00490836c7e112e2b7edbb16c1959
Filipe Mendes <filipemendes@Filipes-MacBook-Pro.local>
2020-07-07 00:29:30 +0100

    (feat) add scroll animation on timeline

--- src/js/timeline.js
+++ src/js/timeline.js
@@ -4,3 +4,35 @@ const scrollToSocial = () => {
     block: 'start'
   });
 };
+
+document.addEventListener('DOMContentLoaded', () => {
+  const topSign = document.getElementsByClassName('timeline__top-sign')[0];
+  const items = document.getElementsByClassName('timeline__date');
+  const scrollHeightTimeline = topSign.parentElement.clientHeight;
+  const topTimeline = topSign.parentElement.offsetTop;
+  const topLimit = topTimeline - 200;
+  const bottomLimit = topLimit + scrollHeightTimeline;
+  const opacityColor = 'rgba(41, 49, 50, .5)';
+  const normalColor = 'rgba(41, 49, 50, 1)';
+
+  document.addEventListener('scroll', () => {
+    if (window.scrollY < topLimit || window.scrollY > bottomLimit) {
+      topSign.style.top = `${window.scrollY < topLimit ? 0 : scrollHeightTimeline}px`;
+      return;
+    }
+
+    // adapt scrollTop of topSign
+    topSign.style.top = `${window.scrollY - topLimit}px`;
+
+    // adapt containers opacity
+    [...items].forEach(item => {
+      if (item) {
+        item.style.color = opacityColor;
+        item.nextElementSibling.style.opacity = .5;
+      }
+    });
+    const itemIdx = Math.floor(Number.parseInt(topSign.style.top) / 218);
+    items[itemIdx].style.color = normalColor;
+    items[itemIdx].nextElementSibling.style.opacity = 1;
+  });
+});
