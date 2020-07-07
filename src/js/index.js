8a6715ab4401878cccaac6493542fcb6d63ea671
Filipe Mendes <filipemendes@Filipes-MacBook-Pro.local>
2020-07-07 03:03:32 +0100

    (fix) bar and top sign positions on timeline

--- src/js/index.js
+++ src/js/index.js
@@ -2,22 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
   document.getElementsByClassName('content')[0].style.opacity = 1;
   document.getElementsByClassName('navbar__filler')[0].style.opacity = 1;
 
-  setWidthCSSVariable();
-  trackWindowsWidth();
   watchNavbarPosition();
   randomQuote();
 });
-
-const trackWindowsWidth = () => {
-  rxjs.fromEvent(window, 'resize', { passive: true })
-    .pipe(
-      rxjs.operators.distinctUntilChanged(),
-    ).subscribe(() => setWidthCSSVariable());
-};
-
-const setWidthCSSVariable = () => {
-  document.documentElement.style.setProperty('--window-width', `${window.innerWidth}px`);
-};
 
 const watchNavbarPosition = () => {
   const navbar = document.getElementsByClassName('navbar')[0];
--- src/js/timeline.js
+++ src/js/timeline.js
@@ -1,13 +1,14 @@
 const scrollToSocial = () => {
-  document.getElementsByClassName('social__container')[0].scrollIntoView({
-    behavior: 'smooth',
-    block: 'start'
-  });
+  const timelineTitle = document.querySelector('.social__container').previousElementSibling.previousElementSibling;
+  window.scroll({ top: timelineTitle.offsetTop - 106 });
 };
 
-document.addEventListener('DOMContentLoaded', () => {
+/** Handle topSign on scroll */
+const handleTopSignScroll = () => {
   const topSign = document.getElementsByClassName('timeline__top-sign')[0];
   const items = document.getElementsByClassName('timeline__date');
+  const firstItem = document.querySelector('.timeline__date');
+
   const scrollHeightTimeline = topSign.parentElement.clientHeight;
   const topTimeline = topSign.parentElement.offsetTop;
   const topLimit = topTimeline - 200;
@@ -15,24 +16,63 @@ document.addEventListener('DOMContentLoaded', () => {
   const opacityColor = 'rgba(41, 49, 50, .5)';
   const normalColor = 'rgba(41, 49, 50, 1)';
 
-  document.addEventListener('scroll', () => {
-    if (window.scrollY < topLimit || window.scrollY > bottomLimit) {
-      topSign.style.top = `${window.scrollY < topLimit ? 0 : scrollHeightTimeline}px`;
-      return;
-    }
+  // adapt scrollTop of topSign
+  rxjs.fromEvent(window, 'scroll', { passive: true })
+    .pipe(
+      rxjs.operators.distinctUntilChanged(),
+    ).subscribe(() => {
+      if (window.scrollY < topLimit) {
+        topSign.style.top = `${firstItem.offsetTop}px`;
+      } else if (window.scrollY > bottomLimit) {
+        topSign.style.top = `${firstItem.offsetTop + scrollHeightTimeline}px`;
+      } else {
+        topSign.style.top = `${firstItem.offsetTop + window.scrollY - topLimit}px`;
+      }
 
-    // adapt scrollTop of topSign
-    topSign.style.top = `${window.scrollY - topLimit}px`;
-
-    // adapt containers opacity
-    [...items].forEach(item => {
+      // adapt containers opacity
+      [...items].forEach(item => {
+        if (item) {
+          item.style.color = opacityColor;
+          item.nextElementSibling.style.opacity = .5;
+        }
+      });
+      const item = items[Math.floor((Number.parseInt(topSign.style.top) - firstItem.offsetTop) / items[1].clientHeight)];
       if (item) {
-        item.style.color = opacityColor;
-        item.nextElementSibling.style.opacity = .5;
+        item.style.color = normalColor;
+        item.nextElementSibling.style.opacity = 1;
       }
     });
-    const itemIdx = Math.floor(Number.parseInt(topSign.style.top) / 218);
-    items[itemIdx].style.color = normalColor;
-    items[itemIdx].nextElementSibling.style.opacity = 1;
+}
+
+const handleTopSignPosition = () => {
+  const topSign = document.getElementsByClassName('timeline__top-sign')[0];
+  const firstItem = document.querySelector('.timeline__date');
+
+  topSign.style.top = `${firstItem.offsetTop}px`;
+  topSign.style.left = `${Math.floor(firstItem.getBoundingClientRect().x + firstItem.getBoundingClientRect().width - 6)}px`;
+}
+
+const handleSeparator = () => {
+  const separator = document.querySelector('.timeline__separator');
+  const nextElement = separator.nextElementSibling;
+  console.log(nextElement);
+  const setSeparatorPosition = (top) => separator.style.top = `${top}px`;
+  setSeparatorPosition(nextElement.offsetTop - separator.clientHeight);
+
+  rxjs.fromEvent(window, 'resize', { passive: true })
+    .pipe(
+      rxjs.operators.distinctUntilChanged(),
+      rxjs.operators.debounceTime(200),
+    ).subscribe(() => {
+      setSeparatorPosition(nextElement.offsetTop - separator.clientHeight);
+      handleTopSignPosition();
+    });
+}
+
+if (window.location.href.endsWith('about.html')) {
+  document.addEventListener('DOMContentLoaded', () => {
+    handleTopSignPosition();
+    handleTopSignScroll();
+    handleSeparator();
   });
-});
+}
